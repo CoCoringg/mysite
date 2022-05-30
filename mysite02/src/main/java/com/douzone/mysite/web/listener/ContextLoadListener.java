4b1b51bf6faad91ae57e54a1b69207c3768a8846
chaewon <rlacodnjs98@naver.com>
2022-05-30 11:40:00 +0900

    ContextLoadListener

--- mysite02/src/main/java/com/douzone/mysite/web/listener/ContextLoadListener.java
+++ mysite02/src/main/java/com/douzone/mysite/web/listener/ContextLoadListener.java
@@ -1,11 +1,15 @@
 package com.douzone.mysite.web.listener;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class ContextLoadListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce)  { 
-    	System.out.println("Application starts....");
+    	ServletContext sc = sce.getServletContext();
+    	String contextConfigLocation = sc.getInitParameter("contextConfigLocation");
+    	
+    	System.out.println("Application starts...." + contextConfigLocation);
     }
     
     public void contextDestroyed(ServletContextEvent sce)  {
