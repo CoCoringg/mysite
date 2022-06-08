74c8331451b89909fc5e4c844371111e329d70cb
chaewon <rlacodnjs98@naver.com>
2022-06-08 16:22:37 +0900

    mybatis #2 - user

--- mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -1,12 +1,5 @@
 package com.douzone.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -17,161 +10,21 @@ import com.douzone.mysite.vo.UserVo;
 public class UserRepository {
 	@Autowired
 	private SqlSession sqlSession;
-	@Autowired
-	private DataSource dataSource;
 	
 	public boolean insert(UserVo vo) {
 		return sqlSession.insert("user.insert", vo) == 1;
 	}
 
 	public UserVo findByEmailAndPassword(UserVo vo) {
-		UserVo result = null;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-
-			connection = dataSource.getConnection();
-
-			String sql = "select no, name"
-					+ "	from user"
-					+ " where email = ?"
-					+ "	and password = ?";
-			pstmt = connection.prepareStatement(sql);
-			pstmt.setString(1, vo.getEmail());
-			pstmt.setString(2, vo.getPassword());
-
-			rs = pstmt.executeQuery();
-
-			if(rs.next()) {
-				Long no = rs.getLong(1); 
-				String name = rs.getString(2);
-
-				result = new UserVo();
-				result.setNo(no);
-				result.setName(name);
-			}
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
+		return sqlSession.selectOne("user.findByEmailAndPassword", vo);
 	}
 
 	public UserVo findByNo(Long num) {
-		UserVo result = null;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-
-			connection = dataSource.getConnection();
-
-			String sql = "select no, name, email, gender"
-					+ "	from user"
-					+ " where no=?";
-			pstmt = connection.prepareStatement(sql);
-			pstmt.setLong(1, num);
-
-			rs = pstmt.executeQuery();
-
-			if(rs.next()) {
-				Long no = rs.getLong(1); 
-				String name = rs.getString(2);
-				String email = rs.getString(3);
-				String gender = rs.getString(4);
-
-				result = new UserVo();
-				result.setNo(no);
-				result.setName(name);
-				result.setEmail(email);
-				result.setGender(gender);
-			}
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
+		return sqlSession.selectOne("user.findByNo", num);
 	}
 
 	public boolean update(UserVo vo) {
-		boolean result = false;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			connection = dataSource.getConnection();
-			
-			if(vo.getPassword() == "") {
-				String sql = "update user"
-						+ "	set name = ?,"
-						+ "        gender = ?"
-						+ "	where no = ?"; 
-				pstmt = connection.prepareStatement(sql);
-				
-				pstmt.setString(1, vo.getName());
-				pstmt.setString(2, vo.getGender());
-				pstmt.setLong(3, vo.getNo());
-				
-			} else {
-				String sql = "update user"
-						+ "	set name = ?,"
-						+ "		password = ?,"
-						+ "        gender = ?"
-						+ "	where no = ?"; 
-				pstmt = connection.prepareStatement(sql);
-				
-				pstmt.setString(1, vo.getName());
-				pstmt.setString(2, vo.getPassword());
-				pstmt.setString(3, vo.getGender());
-				pstmt.setLong(4, vo.getNo());
-			}
-			
-			int count = pstmt.executeUpdate();
-			result = count == 1;
-			
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
+		return sqlSession.update("user.update", vo)==1;
 	}
 }
 
